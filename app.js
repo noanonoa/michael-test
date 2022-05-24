cab715b24888b6a75bffac35b23a6815b8548bf8
Noa Kim <noanonoa@gmail.com>
2022-05-24 15:30:34 -0500

    test example

---
+++ app.js
@@ -0,0 +1,7 @@
+let firstChild = document.querySelector('.child-one')
+let secondChild = document.querySelector('.child-two')
+let thirdChild = document.querySelector('.child-three')
+
+console.log('firstChild variable:', firstChild);
+console.log('secondChild variable:', secondChild);
+console.log('thirdChild variable:', thirdChild);
